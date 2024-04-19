961b311de13aea6fa650226a3b062c68d63abfdd
SunDuckz <gui.bampi0@gmail.com>
2024-04-18 21:57:13 -0300

    criada a tela de conta e criação do projeto

---
+++ WinFormsAppAlgoritimo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WinFormsAppAlgoritimo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Showcard Gothic", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(44, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(773, 209);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Cadastro de conta";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Numero da conta";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(330, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(190, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Titular da conta";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(28, 77);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 30);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "2121";
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(330, 77);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(264, 30);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "Guilherme Bampi";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(330, 159);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(264, 30);
+            textBox3.TabIndex = 7;
+            textBox3.Text = "Corrente";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.Lime;
+            textBox4.Location = new Point(28, 159);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 30);
+            textBox4.TabIndex = 6;
+            textBox4.Text = "3.50";
+            textBox4.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Showcard Gothic", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(330, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(155, 23);
+            label3.TabIndex = 5;
+            label3.Text = "Tipo da Conta";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(28, 131);
+            label4.Name = "label4";
+            label4.Size = new Size(170, 23);
+            label4.TabIndex = 4;
+            label4.Text = "Saldo da conta";
+            label4.Click += label4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(838, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+    }
+}
